c66ac92cde5824e428930a4a157313b1f5436b3a
Sahil Sachdeva <97912110+sahilsachdeva1109@users.noreply.github.com>
2026-08-22 15:03:27 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int len1 = s.length();
+        int len2 = t.length();
+
+        if(len1!= len2){
+            return false;
+        }
+        int a[] = new int[26];
+        for(int i=0; i<len1; i++){
+            a[s.charAt(i)-'a']++;
+        }
+        for(int i =0 ; i<len2; i++){
+            a[t.charAt(i)-'a']--;
+            if(a[t.charAt(i)-'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
